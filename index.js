af9305bcbb5a6713c918390aa83aae9b3d46a69c
Mike <wellwind@mail2000.com.tw>
2022-07-29 10:19:27 +0800

    demo

---
+++ index.js
@@ -0,0 +1,5 @@
+import { methodA } from './module-a';
+import { methodB } from './module-b';
+
+console.log(methodA());
+console.log(methodB());
---
+++ module-a.js
@@ -0,0 +1,3 @@
+export function methodA() {
+    return 'A';
+}
